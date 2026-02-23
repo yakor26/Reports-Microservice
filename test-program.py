11b3a6c004dab030e36e4d2f52ef721a6f2fe11d
abisolakorede <156990606+yakor26@users.noreply.github.com>
2026-02-23 17:11:29 -0500

    create generic test program

---
+++ test-program.py
@@ -0,0 +1,28 @@
+import requests
+
+class Book:
+    def __init__(self, bookID, title, finished_date, pages, rating):
+        self._bookID = bookID,
+        self._title = title,
+        self._finished_date = finished_date,
+        self._pages = pages,
+        self._rating = rating
+
+
+test_data = {
+    1: {},
+    2: {},
+    3: {},
+    4: {},
+    5: {}
+}
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
+
+ 
