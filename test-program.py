83c7e73e7b324a1d8f64190a3438a73e193338c9
abisolakorede <156990606+yakor26@users.noreply.github.com>
2026-02-23 17:17:29 -0500

    create simple prompt

--- test-program.py
+++ test-program.py
@@ -2,20 +2,29 @@ import requests
 
 class Book:
     def __init__(self, bookID, title, finished_date, pages, rating):
-        self._bookID = bookID,
         self._title = title,
         self._finished_date = finished_date,
         self._pages = pages,
         self._rating = rating
 
 
-test_data = {
-    1: {},
-    2: {},
-    3: {},
-    4: {},
-    5: {}
-}
+test_data = [
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+]
+
+
+def prompt():
+    book_title = input("Enter Book Title: ")
+    book_author = input("Enter Author of Book: ")
+    book_finished_date = input("Enter date book finished (mm/dd/yyyy): ")
+    book_pages = input("Enter Number of Pages in Book: ")
+    book_rating = input("Enter your Rating for the Book(1-5): ")
+
 
 def main():
     pass
